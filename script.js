e36f4e10502c9d7ac6b3f34c7a6abed5e928bd24
risho2 <ryo.otk22@gmail.com>
2026-02-03 16:31:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ window.onload = function() {
     document.getElementById('consentButton').addEventListener('click', function() {
         // GoogleフォームのURLリスト
         var forms = [
-            "https://docs.google.com/forms/d/e/1FAIpQLSe0husm3YMPwc1iPoWXZC4PI8ZCQlJX84gzsyh-y-4m2Uu2OA/viewform?usp=sf_link",
-            "https://docs.google.com/forms/d/e/1FAIpQLSdlXhlMS-ZY_aHszvTBAKQBwBc7S9Vzp0y9eMbgnDDVeDv4uA/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLScow7vdv47rHXfxSnKJVsC4G5A1LqOtkvV6G8tQ6cRSYESzBw/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSeMgSjTFW65omoB6lCnEr4eukgoMrvTcnaf9Ai1OPighJG7jQ/viewform?usp=header",
-            "https://docs.google.com/forms/d/e/1FAIpQLSdj2MGcIfWfm5SkfJB-TdUPqUBhE-Hh9e2MEUlC1AsQZg3bmQ/viewform?usp=header"
+            "https://docs.google.com/forms/d/e/1FAIpQLSfvgKdkpbo5S2midiFM2IWQmLp_NMOxMDpUTaIWq8sc9FnJEA/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSe6oykAyBTFuOgrpLdO1URF6IAQZ2RSbrhzQXEA0IUBuesuZQ/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfABvu8eE1ZU06gHe9q2ApN5fS5dhut8UV7kL-eeTJ0b_pfzQ/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSdU3XdV5mQDZVirqjupO6MGMj1CJ0j25phUWrB8BJclqJFT7w/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfuw1urbqdQZeqLiJTf8YinQ5yxSlIh3am9c3cAdEcVjRTEHw/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSeACM6YVASGPMu5JLLiu3BVqXhIZGey0idLFAQJIodlk77SDA/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSdRslhTRapdJrpY_45XvymIN75TAfhc3G0NJ0pj5Afslonj6A/viewform?usp=header",
+            "https://docs.google.com/forms/d/e/1FAIpQLSfVsdsRLRwGTHEtAi9BChYBYm4iAxdwmFgBrtVZJuSvm4YAgQ/viewform?usp=header"
         ];
 
         // ランダムなURLを選択
@@ -17,3 +20,4 @@ window.onload = function() {
         window.location.href = randomFormUrl;
     });
 };
+
